8f1ea87808f153a7e32165aaaf7011b5ad1a08bd
Wladison-Maciel <franciscowladison97@gmail.com>
2025-05-21 16:58:18 -0300

    Criando um contador

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-function App() {
+import { useState } from "react";
+import Contador from "./components/Contador/Contador";
+
+export default function App() {
+
+  // const [situacao, setSituacao] = useState(true)
+
+  // function alternarSituacao() {
+  //   setSituacao(!situacao)
+  // }
+
+  
 
 
   return (
     <>
+      {/* <h2>Situação: {situacao ? "Ativo" : "Inativo"}</h2>
+      <button onClick={alternarSituacao}>Mudar Situação</button> */}
+      <Contador></Contador>
 
     </>
-  )
+  );
 }
-
-export default App
---
+++ src/components/Contador/Contador.jsx
@@ -0,0 +1,29 @@
+import './Contador.css'
+import { useState } from 'react'
+
+export default function Contador() {
+    const [contador, setNumero] = useState(0);
+
+    function incrementarNumero() {
+        setNumero(contador + 1);
+    };
+
+    function decrementarNumero() {
+        setNumero(contador - 1);
+    };
+    function zerarNumero() {
+        setNumero(0);
+    };
+
+    return (
+        <div className='box'>
+            <span>{contador}</span>
+            <div className='botoes'>
+                <button className='botao' onClick={decrementarNumero}>-</button>
+                <button onClick={zerarNumero}>reset</button>
+                <button className='botao' onClick={incrementarNumero}>+</button>
+            </div>
+
+        </div>
+    )
+}
